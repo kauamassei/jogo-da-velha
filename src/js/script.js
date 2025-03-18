dd5c0e8d5c63560325d675522a63182ce736a728
kauamassei <gameplayultra11@gmail.com>
2025-03-18 20:22:21 -0300

    feat: css

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p")
+
+// contador de jogadas
+
+let player1 = 0;
+let player2 = 0;
+
