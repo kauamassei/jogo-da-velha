37b1b6176b862ec8998d1223e2cab999bbab26aa
kauamassei <gameplayultra11@gmail.com>
2025-03-18 20:39:08 -0300

    feat: select game mode function and click boxes event

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,35 @@ let messageText = document.querySelector("#message p")
 let player1 = 0;
 let player2 = 0;
 
+// click nos boxes
+
+const checkEl = (player1, player2) => {
+    if(player1 == player2) {
+        el = x
+    } else {
+        el = o
+    }
+
+    return el
+}
+
+for(let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", function() {
+        
+        let el = checkEl(player1, player2)
+
+        if(this.childNodes.length == 0) {
+            let cloneEl = el.cloneNode(true);
+
+            this.appendChild(cloneEl);
+
+            //salvando jogada
+            if(player1 == player2) {
+                player1++;
+            } else {
+                player2++;
+            }
+        }
+    });
+}
+
